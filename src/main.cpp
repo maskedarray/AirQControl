33c402b28ea7eadbd381116e1f43142ad6cc1fc0
oskyrep <abdulrahman_098@hotmail.com>
2021-03-26 01:50:26 +0500

    developed preliminary code. ready for testing

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,17 @@ String serverName = "http://47.202.196.192:80/air-data/latest";
 
 int hum, voc, co2, particulate;
 int hum_set, voc_set, co2_set, particulate_set;
+String dtime;
 FirebaseData firebaseData;
+StaticJsonDocument<200> doc;
+/*
+ * Things to get out of doc:
+ * 1. voc
+ * 2. co2
+ * 3. pm25
+ * 4. humid
+ * 
+ */
 
 bool readDataFirebase(){
   if( Firebase.getInt(firebaseData, CLIENT_ID + String("/humidity_set"), hum_set) &
@@ -27,19 +37,62 @@ bool readDataFirebase(){
 }
 
 bool readDataLocal(){
-
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      String serverPath = serverName;
+      http.begin(serverPath.c_str());
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+        deserializeJson(doc, payload);
+        hum = doc["humid"];
+        voc = doc["voc"];
+        co2 = doc["co2"];
+        particulate = doc["pm25"];
+        dtime = doc["timestamp"].as<String>();
+        dtime.replace("T"," ");
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      http.end();
+      return true;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+      return false;
+    }
 }
 
 bool setDataFirebase(){
-
+  if( Firebase.setInt(firebaseData, CLIENT_ID + String("/humidity"), hum) &
+  Firebase.setInt(firebaseData, CLIENT_ID + String("/voc"), voc) &
+  Firebase.setInt(firebaseData, CLIENT_ID + String("/co2"), co2) &
+  Firebase.setInt(firebaseData, CLIENT_ID + String("/particulate"), particulate) &
+  Firebase.setString(firebaseData, CLIENT_ID + String("/time"), dtime)){
+    return true;
+  }
+  return false;
 }
 
-bool setDataLocal(){
-
+void setRelays(){
+  digitalWrite(5, (voc > voc_set)? HIGH:LOW);
+  digitalWrite(5, (co2 > co2_set)? HIGH:LOW);
+  digitalWrite(5, (hum > hum_set)? HIGH:LOW);
+  digitalWrite(5, (particulate > particulate_set)? HIGH:LOW);
 }
 
 void setup() {
   Serial.begin(115200);
+  pinMode(5, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(19, OUTPUT);
+  pinMode(21, OUTPUT);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD); 
   Serial.print("connecting"); 
   while (WiFi.status() != WL_CONNECTED) { 
@@ -52,39 +105,13 @@ void setup() {
   Firebase.setwriteSizeLimit(firebaseData, "tiny");
 
 }
-StaticJsonDocument<200> doc;
+
 void loop() {
+  readDataLocal();
   readDataFirebase();
-  Firebase.setDouble(firebaseData,"/Sensor/data1",369);
-  delay(1000);
-  Firebase.setDouble(firebaseData,"/Sensor/data1",6978);
-  delay(1000);
-  double a;
-  Firebase.getDouble(firebaseData,"/Sensor/data1", a);
-  Serial.println(a);
+  setDataFirebase();
+  setRelays();
+  
   Serial.println("okay");
-  if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
-      String serverPath = serverName;
-      http.begin(serverPath.c_str());
-      int httpResponseCode = http.GET();
-      
-      if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
-        deserializeJson(doc, payload);
-        double res = doc["humid"];
-        Serial.println(res);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-      http.end();
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
+
 }

a1d810876033e5fd351e5274ed35cf16680f789b
oskyrep <abdulrahman_098@hotmail.com>
2021-03-26 01:09:54 +0500

    merged with wificlient

--- src/main.cpp
+++ src/main.cpp
@@ -16,27 +16,27 @@ int hum, voc, co2, particulate;
 int hum_set, voc_set, co2_set, particulate_set;
 FirebaseData firebaseData;
 
-// bool readDataFirebase(){
-//   if( Firebase.getInt(firebaseData, CLIENT_ID + String("/humidity_set"), hum_set) &
-//   Firebase.getInt(firebaseData, CLIENT_ID + String("/voc_set"), voc_set) &
-//   Firebase.getInt(firebaseData, CLIENT_ID + String("/co2_set"), co2_set) &
-//   Firebase.getInt(firebaseData, CLIENT_ID + String("/particulate_set"), particulate_set)){
-//     return true;
-//   }
-//   return false;
-// }
+bool readDataFirebase(){
+  if( Firebase.getInt(firebaseData, CLIENT_ID + String("/humidity_set"), hum_set) &
+  Firebase.getInt(firebaseData, CLIENT_ID + String("/voc_set"), voc_set) &
+  Firebase.getInt(firebaseData, CLIENT_ID + String("/co2_set"), co2_set) &
+  Firebase.getInt(firebaseData, CLIENT_ID + String("/particulate_set"), particulate_set)){
+    return true;
+  }
+  return false;
+}
 
-// bool readDataLocal(){
+bool readDataLocal(){
 
-// }
+}
 
-// bool setDataFirebase(){
+bool setDataFirebase(){
 
-// }
+}
 
-// bool setDataLocal(){
+bool setDataLocal(){
 
-// }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -46,32 +46,27 @@ void setup() {
     Serial.print("."); 
     delay(500); 
   } 
-  // Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  // Firebase.reconnectWiFi(true);
-  // Firebase.setReadTimeout(firebaseData, 1000 * 20);
-  // Firebase.setwriteSizeLimit(firebaseData, "tiny");
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  Firebase.setReadTimeout(firebaseData, 1000 * 20);
+  Firebase.setwriteSizeLimit(firebaseData, "tiny");
 
 }
 StaticJsonDocument<200> doc;
 void loop() {
-  // readDataFirebase();
-  // Firebase.setDouble(firebaseData,"/Sensor/data1",369);
-  // delay(1000);
-  // Firebase.setDouble(firebaseData,"/Sensor/data1",6978);
-  // delay(1000);
-  // double a;
-  // Firebase.getDouble(firebaseData,"/Sensor/data1", a);
-  // Serial.println(a);
-  // Serial.println("okay");
+  readDataFirebase();
+  Firebase.setDouble(firebaseData,"/Sensor/data1",369);
+  delay(1000);
+  Firebase.setDouble(firebaseData,"/Sensor/data1",6978);
+  delay(1000);
+  double a;
+  Firebase.getDouble(firebaseData,"/Sensor/data1", a);
+  Serial.println(a);
+  Serial.println("okay");
   if(WiFi.status()== WL_CONNECTED){
       HTTPClient http;
-
       String serverPath = serverName;
-      
-      // Your Domain name with URL path or IP address with path
       http.begin(serverPath.c_str());
-      
-      // Send HTTP GET request
       int httpResponseCode = http.GET();
       
       if (httpResponseCode>0) {
@@ -87,7 +82,6 @@ void loop() {
         Serial.print("Error code: ");
         Serial.println(httpResponseCode);
       }
-      // Free resources
       http.end();
     }
     else {
